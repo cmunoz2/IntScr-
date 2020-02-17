39d622b3b8f0c231f49560ed723eca2a92bfceb0
Cesar D. Munoz <cmunoz2@butlercc.edu>
2020-02-17 15:10:26 -0600

    class2/17/20

--- Assets/Scripts/LookAtClickPoint.cs
+++ Assets/Scripts/LookAtClickPoint.cs
@@ -38,7 +38,7 @@ public class LookAtClickPoint : MonoBehaviour
         {
             RaycastHit hit;
 
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, 5))
             {
                 transform.LookAt(hit.point);
             }  
